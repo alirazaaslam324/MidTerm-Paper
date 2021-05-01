8063dff185d3f3bcc14c9058b795538f0ab2fa24
Ali raza <61656070+alirazaaslam324@users.noreply.github.com>
2021-05-01 16:52:33 +0500

    FROM QUESTION 2 REQUIREMENT DATABASE MS SQL FILE

---
+++ databae_file_MS_SQL.sql
@@ -0,0 +1,16 @@
+USE [midterm]
+GO
+
+/****** Object:  Table [dbo].[Cities]    Script Date: 5/1/2021 4:51:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Cities](
+	[City_id] [int] NULL,
+	[City_Name] [varchar](150) NULL
+) ON [PRIMARY]
+GO
+
